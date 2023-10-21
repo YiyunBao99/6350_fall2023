fad1d9508d57619cca733a336c0b3e5afa6c0a57
YiyunBao99 <bao.yiy@icloud.com>
2023-10-21 15:27:09 -0700

    Initial Commit

---
+++ StockApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockApp2
+//
+//  Created by Yiyun Bao on 10/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
