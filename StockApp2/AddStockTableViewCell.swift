7d06f8b289b5fe241e4f68d7da9a9f5a76d433a1
YiyunBao99 <bao.yiy@icloud.com>
2023-10-23 23:19:25 -0700

    Stock App Project

---
+++ StockApp2/AddStockTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AddStockTableViewCell.swift
+//  StockApp2
+//
+//  Created by Yiyun Bao on 10/21/23.
+//
+
+import UIKit
+
+class AddStockTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ StockApp2/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp2
+//
+//  Created by Yiyun Bao on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "2e359aa471b4651680e86d91915402ed"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
